868dbcabe3d3463d58cc1f347cbb2b79361af1b0
southveter <southveter@gmail.com>
2020-04-07 16:21:29 +0300

    Настроил сборку, разбил на модули

---
+++ src/components/day.js
@@ -0,0 +1,11 @@
+export const createMokiTripDays = () => {
+  return (
+    `<ul class="trip-days">
+      <li class="trip-days__item  day">
+        <div class="day__info"></div>
+        <ul class="trip-events__list">
+        </ul>
+      </li>
+    </ul>`
+  );
+};
